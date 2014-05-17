5c76b3b27a95e3fdbcfb827fcfe5d0d6436b2b7a
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-05-17 14:17:09 +0700

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,3 @@
 function nav(t){var y='://',t=t.split(y),s=t[0],u=t[1].split('/'),d=[],e=[];for(t=0;t<u.length;)e.push((e[t-1]||s+y)+'/'+u[t]),d.push(u[t++]);return[d,e]}
-var D=document.body.insertBefore(document.createElement('hr'),document.body.children[0]),URL=nav(document.URL);
+var D=document.body.insertBefore(document.createElement('div'),document.body.children[0]),URL=nav(document.URL);
 for(var p=0;p<URL[0].length;p++)D.innerHTML+='&gt;&nbsp;<a href="'+URL[1][p]+'">'+URL[0][p]+'</a>&nbsp;';
